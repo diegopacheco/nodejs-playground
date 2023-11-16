27ee912c2445b029fce099a9f67b8a6dcebae516
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-15 23:38:00 -0800

    progress

---
+++ v8-profiler/src/index.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+users = [];
+app.get('/newUser', (req, res) => {
+    let username = req.query.username || '';
+    const password = req.query.password || '';
+    username = username.replace(/[!@#$%^&*]/g, '');
+    if (!username || !password || users[username]) {
+    return res.sendStatus(400);
+    }
+    const salt = crypto.randomBytes(128).toString('base64');
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
+    users[username] = { salt, hash };
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
