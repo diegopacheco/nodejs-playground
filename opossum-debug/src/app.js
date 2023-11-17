313efe3562e539d598e7d1ed5bffa7e72f4a543b
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-16 21:37:18 -0800

    debugging a stinky animal.js

---
+++ opossum-debug/src/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+const CircuitBreaker = require('opossum');
+
+function asyncFunctionThatCouldFail(x, y) {
+    return new Promise((resolve, reject) => {
+      console.log("*** here!");
+      console.log(resolve);
+      console.log(reject);
+    });
+};
+
+function dummyProc(){
+    if (1+1==2){
+        console.log("ok");
+    }
+};
+
+app.get('/', (req, res) => {
+    
+    const options = {
+        timeout: 3000,                // If our function takes longer than 3 seconds, trigger a failure
+        errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+        resetTimeout: 30000           // After 30 seconds, try again.
+    };
+    const breaker = new CircuitBreaker(asyncFunctionThatCouldFail, options);
+    
+    breaker.fire(dummyProc())
+        .then(console.log)
+        .catch(console.error);
+
+    res.send('Hello World stinky animal!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
