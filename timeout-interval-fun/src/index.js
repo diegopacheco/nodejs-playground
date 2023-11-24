3f31b3f9d43112ff07947d82c89c5f18a8d1cf0f
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-23 23:35:37 -0800

    settimeout and setinterval

---
+++ timeout-interval-fun/src/index.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    // runs after 2 seconds
+    console.log("just run after 2s ");
+}, 2000);
+
+setInterval(() => {
+    // runs every 3 seconds
+    console.log("3s has passed!");
+  }, 3000);
