5cb0d14ea33b82d714b00b406111d7609e3b8a41
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-07 22:07:12 -0800

    node trap

--- js-trap-nan/test.sh
+++ js-trap-nan/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 for i in {16..23}
 do
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-    
     nvm install $i
     nvm use $i
+    echo "Running index.js with Node.js version $i..."
     echo "Node Version: $(node -v)"
     node index.js
 done
